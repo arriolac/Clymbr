6139c8e5569e51ea0c57fef1f6e63f564a9f5cd1
Chris Arriola <arriolac279@gmail.com>
2012-12-08 15:04:55 -0800

    Adding rails back-end

---
+++ back-end/clymbr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clymbr::Application.config.secret_token = '9b3161a01ee52b16936b33076e64bb72340f0bc045c5f7f8ab1d15850ece6975dcaaa00f61dfa15663c741235812803bb28022a393d503cc33e543cdf0144793'
