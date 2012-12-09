bd6a06332815edff399c19e54ddaa4f4a319ee68
Chris Arriola <arriolac279@gmail.com>
2012-12-09 15:37:43 -0800

    More profile info.

--- public/js/views/profile/ProfileEditView.js
+++ public/js/views/profile/ProfileEditView.js
@@ -7,9 +7,13 @@ define([
     'backbone',
     'text!templates/profileEditTemplate.html'
 ], function($, Backbone, profileEditTemplate) {
+
+    var ENTER_KEY_CODE = 13;
+
     var ProfileEditView = Backbone.View.extend({
         events: {
-            "submit form#profile-edit-form": "edit"
+            "submit form#profile-edit-form": "save",
+            "keypress #new-climbing-locations": "createLocationOnEnter"
         }, 
 
         el: ".content",
@@ -23,10 +27,21 @@ define([
             this.delegateEvents();
         },
 
-        logIn: function(e) {
+        //
+        // Events
+        //
+        save: function(e) {
             // TODO: after the casal builds
             console.log("profile-edit");
         },
+
+        createLocationOnEnter: function(e) {
+
+            if (e.keyCode != ENTER_KEY_CODE) return;
+
+            this.input.val(''); 
+            console.log("blah blah blah");
+        }
     });
 
     return ProfileEditView;
